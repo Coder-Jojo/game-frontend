30319eaf94f27826ca4f63a8f0c9a3475f117e02
nikhil <nikhilch@iitbhilai.ac.in>
2024-01-08 20:31:07 +0545

    made loading screen better

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, { useState } from "react";
 import io from "socket.io-client";
 import { Home, GameArea } from "./components";
 import SocketContext from "./socketContext";
+import LoadingPage from "./loading";
 
 // const socket = io("http://localhost:5000");
 // const socket = io("https://jojo-game-1.herokuapp.com");
 const socket = io("https://jojo-game-backend.onrender.com");
+socket.connect();
+var forcedDelay = true;
+setTimeout(() => forcedDelay = false, 3000);
 
 const App = () => {
   const [inGame, setInGame] = useState(false);
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-
-  if (!socket) {
-    return <div>Loading...</div>;
+  const [loading, setLoading] = useState(socket.connected && !forcedDelay);
+  const [reRender, setReRender] = useState(1);
+  
+  if (!loading || reRender) {
+    setTimeout(() => {
+      setLoading(socket.connected && !forcedDelay)
+      if (!loading) setReRender(reRender + 1);
+      else setReRender(0);
+    }, 1000);
+    console.log("loading");
+    return <LoadingPage />;
   }
 
   return (
